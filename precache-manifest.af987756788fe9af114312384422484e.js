e33bd71ff5489c2137edeaafe2683d8405a7095e
Amit Shukla <amit090shukla@gmail.com>
2018-12-05 12:57:44 -0800

    Updates

---
+++ precache-manifest.af987756788fe9af114312384422484e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "fce45a49e1ba609d6662",
+    "url": "/portfolio/static/js/main.fce45a49.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/portfolio/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "fce45a49e1ba609d6662",
+    "url": "/portfolio/static/css/main.810447e3.chunk.css"
+  },
+  {
+    "revision": "7996530c71024b062b9c0a1796d09f13",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f73e6da9602d7f876169d334ca5744b9.js"
+  "/portfolio/precache-manifest.af987756788fe9af114312384422484e.js"
 );
 
 workbox.clientsClaim();
